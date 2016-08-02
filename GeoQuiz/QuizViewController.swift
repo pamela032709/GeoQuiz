253ec42c7e111b3c876cf832a4df59361e48b7d3
pamela032709 <iDevelopers@Pamelas-iMac.local>
2016-08-02 13:03:58 -0400

    quiz #1
    Project [added if statement  by pamela rios]

--- GeoQuiz/QuizViewController.swift
+++ GeoQuiz/QuizViewController.swift
@@ -37,5 +37,14 @@ class QuizViewController: UIViewController {
     @IBAction func flagButtonPressed(sender: UIButton) {
 
         // TODO: Add code to display a message to the user telling them whether or not they guessed correctly.
+
+    if sender.tag == correctButtonTag{
+    displayAlert("Correct" , messageText: "Good Job!") }
+    
+    else {
+    displayAlert("Incorrect",messageText: "Nah, Try again")
+    
     }
+    
+}
 }
